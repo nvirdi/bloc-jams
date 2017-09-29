804260df47690358fe5965b83aa2dbb960dd6faf
Navnit Virdi <nvirdi3@gmail.com>
2017-09-29 16:29:25 -0700

    Checkpoint 29 Completed

--- scripts/album.js
+++ scripts/album.js
@@ -54,7 +54,7 @@
       + '</tr>'
       ;
 
-     return template;
+     return $(template);
  };
 
  var albumTitle = document.getElementsByClassName('album-view-title')[0];
@@ -64,19 +64,25 @@
  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
  var setCurrentAlbum = function(album) {
+    var $albumTitle = $('.album-view-title');
+    var $albumArtist = $('.album-view-artist');
+    var $albumReleaseInfo = $('.album-view-release-info');
+    var $albumImage = $('.album-cover-art');
+    var $albumSongList = $('.album-view-song-list');
 
      // #1
-     albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+     $albumTitle.text(album.title);
+     $albumArtist.text(album.artist);
+     $albumReleaseInfo.text(album.year + ' ' + album.label);
+     $albumImage.attr('src', album.albumArtUrl);
 
      // #2
-     albumSongList.innerHTML = '';
+     $albumSongList.empty();
 
      // #3
      for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+       var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+       $albumSongList.append($newRow);
      }
  };
 
