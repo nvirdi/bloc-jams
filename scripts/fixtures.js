67c28760c6ea3eab252bc3773c1b11c43f55166a
Navnit Virdi <nvirdi3@gmail.com>
2017-10-02 17:12:13 -0700

    checkpoint 31 completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+ var albumDrake = {
+     title: 'More Life',
+     artist: 'Drake',
+     label: 'OVO',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/12.png',
+     songs: [
+         { title: 'For Love', duration: '4:26' },
+         { title: 'Glow', duration: '3:14' },
+         { title: 'Passionfruit', duration: '5:01' },
+         { title: 'Free Smoke', duration: '3:21'},
+         { title: 'Portland', duration: '2:15'}
+       ]
+  };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
